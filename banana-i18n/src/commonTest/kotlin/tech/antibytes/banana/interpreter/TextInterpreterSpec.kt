cc20a068f8ab8584e8d023f0d6faaedf0d17b92b
Matthias Geisler <matthias.geisler@data4life.care>
2021-12-27 06:28:51 +0100

    Add Text Interpreter

---
+++ banana-i18n/src/commonTest/kotlin/tech/antibytes/banana/interpreter/TextInterpreterSpec.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by LGPL v2.1
+ */
+
+package tech.antibytes.banana.interpreter
+
+import tech.antibytes.banana.BananaContract
+import tech.antibytes.banana.ast.TextNode
+import tech.antibytes.mock.interpreter.TextInterceptorSpy
+import tech.antibytes.util.test.fulfils
+import tech.antibytes.util.test.mustBe
+import kotlin.test.AfterTest
+import kotlin.test.Test
+
+class TextInterpreterSpec {
+    private val interceptor = TextInterceptorSpy()
+
+    @AfterTest
+    fun tearDown() {
+        interceptor.clear()
+    }
+
+    @Test
+    fun `It fulfils InterpreterPlugin`() {
+        TextInterpreter(interceptor) fulfils BananaContract.InterpreterPlugin::class
+    }
+
+    @Test
+    fun `Given interpret is called with a TextNode it returns the Nodes value concartinated`() {
+        // Given
+        val chunks = listOf("A", " ", "B", " ", "C")
+
+        // When
+        val result = TextInterpreter(interceptor).interpret(TextNode(chunks))
+
+        // Then
+        result mustBe chunks.joinToString("")
+    }
+
+    @Test
+    fun `Given interpret is called with a TextNode it delegates the Nodes value to the TextInterceptor`() {
+        // Given
+        val chunks = listOf("A", " ", "B", " ", "C")
+        val mapping = mapOf(
+            "A" to "D",
+            "B" to "E",
+            "C" to "F"
+        )
+        interceptor.intercept = { chunk -> mapping.getOrElse(chunk) { " " } }
+
+        // When
+        val result = TextInterpreter(interceptor).interpret(TextNode(chunks))
+
+        // Then
+        result mustBe mapping.values.joinToString(" ")
+    }
+}
