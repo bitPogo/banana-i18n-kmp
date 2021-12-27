b611b3c9f9642c76d15b5b6afb3aa599a56d72c6
Matthias Geisler <matthias.geisler@data4life.care>
2021-12-27 06:38:38 +0100

    Add default TextInterceptor

---
+++ banana-i18n/src/commonTest/kotlin/tech/antibytes/banana/interpreter/DefaultTextInterceptorSpec.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by LGPL v2.1
+ */
+
+package tech.antibytes.banana.interpreter
+
+import com.appmattus.kotlinfixture.kotlinFixture
+import tech.antibytes.banana.BananaContract
+import tech.antibytes.util.test.fulfils
+import tech.antibytes.util.test.mustBe
+import kotlin.test.Test
+
+class DefaultTextInterceptorSpec {
+    private val fixture = kotlinFixture()
+
+    @Test
+    fun `It fulfils TextInterceptor`() {
+        DefaultTextInterceptor() fulfils BananaContract.TextInterceptor::class
+    }
+
+    @Test
+    fun `Given intercept is called with a String it reflects it`() {
+        // Given
+        val chunk: String = fixture()
+
+        // When
+        val result = DefaultTextInterceptor().intercept(chunk)
+
+        // Then
+        result mustBe chunk
+    }
+}
