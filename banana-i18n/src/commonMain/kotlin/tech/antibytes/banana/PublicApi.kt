c6e4522879934c4d068f8155a87d259d63b8b87f
Matthias Geisler <matthias.geisler@data4life.care>
2021-12-27 13:11:53 +0100

    TODO

---
+++ banana-i18n/src/commonMain/kotlin/tech/antibytes/banana/PublicApi.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by LGPL v2.1
+ */
+
+package tech.antibytes.banana
+
+interface PublicApi {
+}
