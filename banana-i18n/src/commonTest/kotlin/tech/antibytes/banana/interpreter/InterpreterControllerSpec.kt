d573fde13caeca67aa0131327342780b020e3ec6
Matthias Geisler <matthias.geisler@data4life.care>
2021-12-29 03:24:05 +0100

    Introduce InterpreterController

--- banana-i18n/src/commonTest/kotlin/tech/antibytes/banana/interpreter/InterpreterControllerSpec.kt
+++ banana-i18n/src/commonTest/kotlin/tech/antibytes/banana/interpreter/InterpreterControllerSpec.kt
@@ -6,11 +6,44 @@
 
 package tech.antibytes.banana.interpreter
 
+import com.appmattus.kotlinfixture.kotlinFixture
+import tech.antibytes.banana.BananaContract
+import tech.antibytes.banana.ast.InternalNodes
+import tech.antibytes.mock.interpreter.InterpreterPluginStub
+import tech.antibytes.util.test.fulfils
+import tech.antibytes.util.test.mustBe
 import kotlin.test.Test
 
 class InterpreterControllerSpec {
+    private val fixture = kotlinFixture()
+
     @Test
     fun `It fulfils InterpreterController`() {
-        InterpreterController()
+        InterpreterController(
+            InterpreterPluginStub(),
+        ) fulfils BananaContract.InterpreterController::class
+    }
+
+    @Test
+    fun `Given interpret is called with a TextNode it delegates it to the TextInterpreter`() {
+        // Given
+        val nestedInterpreter = InterpreterPluginStub<InternalNodes.TextNode>()
+        val expected: String = fixture()
+        val givenNode = InternalNodes.TextNode(fixture())
+        var capturedNode: BananaContract.Node? = null
+
+        nestedInterpreter.interpret = { node ->
+            capturedNode = node
+            expected
+        }
+
+        // When
+        val actual = InterpreterController(
+            textInterpreter = nestedInterpreter,
+        ).interpret(givenNode)
+
+        // Then
+        actual mustBe expected
+        capturedNode!! mustBe givenNode
     }
 }
