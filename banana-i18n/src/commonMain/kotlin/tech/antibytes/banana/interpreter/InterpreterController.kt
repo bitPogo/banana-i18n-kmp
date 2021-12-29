3e8c0030e952d15e5efe5e05c3f0a9bdf376e47b
Matthias Geisler <matthias.geisler@data4life.care>
2021-12-29 03:24:05 +0100

    Introduce InterpreterController

--- banana-i18n/src/commonMain/kotlin/tech/antibytes/banana/interpreter/InterpreterController.kt
+++ banana-i18n/src/commonMain/kotlin/tech/antibytes/banana/interpreter/InterpreterController.kt
@@ -6,6 +6,16 @@
 
 package tech.antibytes.banana.interpreter
 
-internal class InterpreterController {
+import tech.antibytes.banana.BananaContract
+import tech.antibytes.banana.ast.InternalNodes
 
+internal class InterpreterController(
+    private val textInterpreter: BananaContract.InterpreterPlugin<InternalNodes.TextNode>,
+) : BananaContract.InterpreterController {
+    override fun interpret(node: BananaContract.Node): String {
+        return when (node) {
+            is InternalNodes.TextNode -> textInterpreter.interpret(node)
+            else -> TODO()
+        }
+    }
 }
